b53d6418bfbd23bb45bdfdc18be20c3368be8bed
dannyhsdl <danwong2018@outlook.com>
2019-01-15 19:50:36 +0800

    示例：对字典的简单操作

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dictionary={'country ':'china',
+            'location ':'asia',
+            'capital':'beijing',
+            'main city':'shanghai,guangzhou',
+            'population':'about 1.4 billion'
+            }
+for k,v in dictionary.items():
+    print(k.title()+':'+v.title())
