cda72801b8ceb12f1f628894868f1ff2da4f5751
dannyhsdl <danwong2018@outlook.com>
2019-01-15 22:19:39 +0800

    定义一个函数

---
+++ functions.py
@@ -0,0 +1,12 @@
+a1='china'
+a2='america'
+a3='russia'
+a4='united nations'
+a5='canada'
+my_list=[a1,a2,a3,a4,a5]
+def m1(list00):
+    print('there are many countries in the list.\n')
+    for i in list00:
+        print(i)
+m1(my_list)
+
