f995310917e78447a296080d66fa97dc9870d241
dannyhsdl <46584587+dannyhsdl@users.noreply.github.com>
2019-07-08 22:05:26 +0800

    A practice for list
    list practice
    You can download it and then practice yourself

---
+++ ex34.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# list practice
+# here we go
+animals=['bear','python','peacock','kangroo','whale','platypus']
+print("1. The animal at 1 is %s"% animals[1]) # here we use the term called Ordinal
+print("2. The 3rd animal is %s"% animals[2]) # here we use the term called Cardinal number
+print("3. The 1st animal is %s"% animals[0])
+print("4. The animal at 3 is %s"% animals[3])
+print("5. The 5th animal is  %s"% animals[4])
+print("6. The animal at 2 is %s"% animals[2])
+print("7. The 6th animal is %s"% animals[5])
+print("8. The animal at 4 is %s"% animals[4])
+
+# Did you know the difference between the Ordinal and the Cardinal numbers?
